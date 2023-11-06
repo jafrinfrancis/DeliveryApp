5a9ac3deb456c028790bdf4c7d9622b1b23832d2
bhanuPalla <bhanupalla1122@gmail.com>
2023-11-06 07:33:30 +0000

    Registerpage

---
+++ dotnetproject/dotnetmvcapp/Controllers/AccountController.cs
@@ -0,0 +1,53 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using dotnetmvcapp.Services;
+using dotnetmvcapp.Models;
+
+
+
+namespace dotnetmvcapp.Controllers
+{
+    [Route("[controller]/[action]")]
+    public class AccountController : Controller
+    {
+        private readonly IAccountService _service;
+        public AccountController(IAccountService service)
+        {
+            _service=service;
+        }
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Register(Register model )
+        {
+            if(ModelState.IsValid)
+            {       var response=await _service.Register(model);
+                    Console.WriteLine("Test 1");
+                    Console.WriteLine(response.ErrorMessage);
+                    if(response!=null)
+                    {
+                        Console.WriteLine("Test 1");
+                        return RedirectToAction("Index","Home");
+                        
+                    }
+                    return View();
+            }
+           
+            return View(model);
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        
+    }
+}
--- dotnetproject/dotnetmvcapp/Models/Register.cs
+++ dotnetproject/dotnetmvcapp/Models/Register.cs
@@ -2,15 +2,33 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace dotnetmvcapp.Models
 {
     public class Register
     {
+        [Required]
+        [Display(Name= "UserName")]
         public string UserName { get; set; }
+         [Required]
+         [EmailAddress]
+         [Display(Name="Email")]
         public string Email { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name="Password")]
         public string Password { get; set; }
+        [Required]
+        [Display(Name="ConfirmPassword")]
+        [DataType(DataType.Password)]
+        [Compare("Password",ErrorMessage="The Password and Confirm Password do not match.")]
+        public string ConfirmPassword { get; set; }
+        [Required]
+        [Display(Name="PhoneNumber")]
+        [RegularExpression(@"^\+?(\d{1,4})?[-.]?(\d{1,14})$",ErrorMessage="Invalid phone number")]
         public string PhoneNumber { get; set; }
+        [Required]
         public int UserRole { get; set; }        
     }
 }
